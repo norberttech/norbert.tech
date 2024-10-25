566d0587e24db1bdf27c2f9dd2440c858137d859
Norbert Orzechowicz <contact@norbert.tech>
2024-10-25 22:47:26 +0200

    Added consulting website

---
+++ src/Controller/ConsultingController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NorbertTech\Portfolio\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class ConsultingController extends AbstractController
+{
+    #[Route('/consulting', name: 'consulting')]
+    public function index(Request $request) : Response
+    {
+        return $this->render('consulting/index.html.twig', []);
+    }
+}
