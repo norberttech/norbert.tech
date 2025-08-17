3b7d8f18dcd8bb7d33013156573d31a7755a8ae4
Norbert Orzechowicz <contact@norbert.tech>
2025-08-17 15:10:13 +0200

    Fixed styles

--- output/assets/@symfony/stimulus-bundle/controllers-30e149621baab8220e8ae00cc40a4a23.js
+++
@@ -1,4 +0,0 @@
-import controller_0 from "../../controllers/hello_controller.js";
-export const eagerControllers = {"hello": controller_0};
-export const lazyControllers = {"clipboard": () => import("../../controllers/clipboard_controller.js"), "syntax-highlight": () => import("../../controllers/syntax_highlight_controller.js")};
-export const isApplicationDebug = true;
