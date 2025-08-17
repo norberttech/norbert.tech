4c574e77a0efdd5f1c96d7fd5d03664ee4998f67
Norbert Orzechowicz <contact@norbert.tech>
2025-08-17 15:11:34 +0200

    Removed github workflow

--- assets/vendor/highlight.js/lib/languages/shell-82883b411c0363fd67d4b2a65c9191cd.js
+++
@@ -1,7 +0,0 @@
-/**
- * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.39.0.
- * Original file: /npm/highlight.js@11.9.0/es/languages/shell.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-function s(s){return{name:"Shell Session",aliases:["console","shellsession"],contains:[{className:"meta.prompt",begin:/^\s{0,3}[/~\w\d[\]()@-]*[>%$#][ ]?/,starts:{end:/[^\\](?=\s*$)/,subLanguage:"bash"}}]}}export{s as default};
